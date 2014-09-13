1dde7d2610cb5503ac98c0262aaa7dea76e8d2e6
Rebecca Anne Sealfon <ras2198@columbia.edu>
2014-09-13 12:53:32 -0400

    Add empty Git repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OkcupidLike::Application.config.secret_key_base = 'b791efab6899afa8b5c4db08308cbcf40cb30ed2118d3a5d3dd2aadc65ea66d9628c2bbed0af2f1094cd4d085937b506a4c7c501fb32885181203cc9639e7c87'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OkcupidLike::Application.config.session_store :cookie_store, key: '_okcupid-like_session'
